06b114651231f05fe040edbe4b97896870239a62
Anurag-init6 <init6story@gmail.com>
2025-09-22 19:56:39 +0530

    My First commit using git

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+# password_gen.py
+import random
+import string
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(chars) for _ in range(length))
+
+if __name__ == "__main__":
+    print("Generated password:", generate_password(16))
+
